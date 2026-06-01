cff69de52c0daf44d11e679e8c4b9ff8f92c4979
Mike Rasell C. Dago-oc <mikerasell123x@gmail.com>
2026-06-01 14:28:45 +0800

    cleaned goated file

--- laravel/resources/js/main.tsx
+++ laravel/resources/js/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import '../css/app.css' 
 
 function App() {
-    return <h1 className="text-3xl font-bold">React + TS + Laravel yawa 🚀</h1>
+    return <h1 className="text-3xl font-bold">Test</h1>
 }
 
 ReactDOM.createRoot(document.getElementById('app')!).render(
